720f00625f5f73fe8b8272226e6b718678bd154d
kevinhtran <kevdeveloperjs@gmail.com>
2021-11-04 03:33:36 -0700

    created coin component with elements

--- src/App.js
+++ src/App.js
@@ -17,11 +17,17 @@ function App() {
         console.log(err);
       });
   }, []);
-  return <div className="App">Hello world</div>;
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input type='text' className='coin-input' placeholder='Search' />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
-
-// Notes:
-// Axios:
-// npm i axios
